d65f03b2b4a7908e23237ede9a3ae8f1faef9129
stan-kempachi <35777602+stan-kempachi@users.noreply.github.com>
2018-09-16 19:51:28 +0200

    Modification du script, fonctionnel
    Réécriture du script complet.

---
+++ Classe.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Ce fichier contient des classes représentant la base de données de ce programme"""
+
+
+class Categories():
+    """Classe représentant la table 'Categories' de la base de données"""
+
+    def __init__(self, data_from_off, index=None):
+        """Initier un dictionnaire pour argument"""
+        #Utiliser la clé lors de la création de la base de données à partir de l'API OFF
+        try:
+            self.id = data_from_off["id"]
+            self.name = data_from_off["categories"]
+        #Utilisez l'index lorsque l'on appel la classe depuis le programme
+        except TypeError:
+            self.id = data_from_off[0]
+            self.name = data_from_off[1]
+            self.index = index
+
+
+class Food():
+    """Classe représentant la table 'Food' de la base de données"""
+    
+    def __init__(self, data_from_off, index=None):    
+        """Initier un dictionnaire pour argument"""
+
+        #Utiliser la clé lors de la création de la base de données à partir de l'API OFF
+        try:
+            self.name = data_from_off ["product_name_fr"] 
+            try:
+                self.category = self.id = data_from_off["id"]
+            except IndexError:
+                pass
+            try:
+                self.nutri_score = data_from_off["nutrition_grade_fr"]
+            except KeyError:
+                self.nutri_score = ""
+            try:
+                self.stores = data_from_off["stores"]
+            except KeyError:
+                self.stores = ""
+            try:
+                self.url = data_from_off["url"]
+            except KeyError:
+                self.url = "Url not available"
+        except IndexError:
+            pass
+
+           #Use index when call the class from the programme
+        except TypeError:
+            self.id = data_from_off[0]
+            self.name = data_from_off[1]
+            self.category = data_from_off[2]
+            self.nutri_score = data_from_off[3]
+            self.stores = ""
+            try:
+                self.stores = data_from_off[4]
+            except IndexError:
+                pass
+            try:
+                self.url = data_from_off[5]
+            except IndexError:
+                self.url = data_from_off[4]
+            self.index = index
+
--- bdd_create.py
+++ bdd_create.py
@@ -1,132 +1,169 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+from typing import List
+
 import pymysql.cursors
 import sys
 import requests, json
 
-#import personnal module
-import classes as cl
+# import personnal module
+from requests import Response
+
+import Classe as cl
 
 FILE_CREATE = """DROP DATABASE IF EXISTS openfoodfacts;
-
 CREATE DATABASE openfoodfacts CHARACTER SET 'utf8';
-
 USE openfoodfacts;
-
 CREATE TABLE IF NOT EXISTS Food(
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	name VARCHAR(150) NOT NULL,
-	categories_id VARCHAR(400) NOT NULL,
-	nutri_score CHAR(1),
-	url VARCHAR(150),
-	stores VARCHAR(150),
-	PRIMARY KEY(id)
-	)ENGINE=InnoDB;
-	
-CREATE TABLE IF NOT EXISTS Categories(
-	id VARCHAR(500) NOT NULL,
-	name VARCHAR(100) NOT NULL,
-	PRIMARY KEY(id)
-	)ENGINE=InnoDB;
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    categories_id VARCHAR(400) NOT NULL,
+    nutri_score CHAR(1),
+    url VARCHAR(150),
+    stores VARCHAR(150),
+    PRIMARY KEY(id)
+    )ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS Categories(
+    id VARCHAR(500) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+    )ENGINE=InnoDB;
 CREATE TABLE IF NOT EXISTS Backup(
-	id INT UNSIGNED AUTO_INCREMENT,
-	product_id INT UNSIGNED NOT NULL,
-	substitute_id INT UNSIGNED NOT NULL,
-	PRIMARY KEY(id)
-	)ENGINE=InnoDB;
-	
+    id INT UNSIGNED AUTO_INCREMENT,
+    product_id INT UNSIGNED NOT NULL,
+    substitute_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY(id)
+    )ENGINE=InnoDB;
+
 """
 
 URL0 = "https://fr.openfoodfacts.org/categorie/aliments-et-boissons-a-base-de-vegetaux/1.json"
 URL2 = "https://fr.openfoodfacts.org/categories.json"
+CATEGORIES = ["https://fr.openfoodfacts.org/categorie/aliments-a-base-de-fruits-et-de-legumes/", \
+              "https://fr.openfoodfacts.org/categorie/biscuits-aperitifs/", \
+              "https://fr.openfoodfacts.org/categorie/boissons/", \
+              "https://fr.openfoodfacts.org/categorie/Boeuf/", \
+              "https://fr.openfoodfacts.org/categorie/Chips/", \
+              "https://fr.openfoodfacts.org/categorie/Desserts/", \
+              "https://fr.openfoodfacts.org/categorie/Fromages/", \
+              "https://fr.openfoodfacts.org/categorie/Legumes-frais/", \
+              "https://fr.openfoodfacts.org/categorie/Patisseries/", \
+              "https://fr.openfoodfacts.org/categorie/Surgeles/"]
 
 
-#connection à la base de données
+nouvelle_liste_url = []  
+url_format = []
+
+
+
+# connection à la base de données
 conn = pymysql.connect(host='localhost', user='root', passwd='', db='Openfoodfacts', charset='utf8')
 cursor = conn.cursor()
 
 
-
-def get_data_from_api(url):
+def recup_data_api(url):
     """Take an url and return data"""
-    data = requests.get(url)
+    data: Response = requests.get(url)
     data.encoding = "utf8"
     return data.json()
+    print(data)
 
 
-def fill_categories_table(url):
-    """Fonction servant à remplir les tableaux de catégories, avec les données de catégories de OFF"""
-    data_from_api = get_data_from_api(URL2)
 
-    for data in data_from_api["tags"]:
-        if data["products"] > 500 and "en:" in data["id"]: #Don't take the categories with to few products and
-            try:
-                category = cl.Categories(data)
-                if "en:" in category.name or "fr:" in category.name:
-                    pass
-                else:
-                    cursor.execute("INSERT INTO Categories (id, name)"\
-                        "VALUES (%s, %s)", (category.id, category.name))
-                    conn.commit()
-                    print("Catégorie correctement insérée")
-            #Don't take the non utf-8 data
-            except conn.OperationalError:     
-                pass
-
-            
-
-def insert_product(url):    
-    data_from_api = get_data_from_api(URL0)
     
-    for data in data_from_api["products"]:
-        try:
-            nutri_values = ["a", "b", "c", "d", "e"]
-            nutri_score = []
-            for nutri_values in data_from_api["nutrition_grade_fr"]:
-                nutri_score.append(data.upper())
-                return food.nutri_score          
-        except KeyError:
-            str_nutr_grade = "N/A"
-            nutriscore = 0
-        try:
-            food = cl.Food(data)
-            cursor.execute("INSERT INTO Food (name, categories_id, nutri_score, url, stores)"\
-                           "VALUES (%s, %s, %s, %s, %s)",
-                           (food.name, food.category, food.nutri_score, food.url, food.stores))
-            conn.commit()
-            print("Produit correctement insérée")            
-        except conn.OperationalError: #Don't take the products with encoding error
-            pass
-        except KeyError: #Don't take lignes without 'product_name'
-            pass
-        except AttributeError: #Don't take products with 0 categories
-            pass
-        except conn.IntegrityError: #Don't take the products with an category unknow in the database
-            pass
-        except conn.DataError: #Pass when product name is too long
-            pass
+
+def remplir_table_categorie():
+    data_from_cat = {}
+    for elt in CATEGORIES:  # pour chaque elt de ma liste
+        url_format.append(elt + (str(".json")))
+    print(url_format)
+    for elt in url_format:
+        data_from_cat = recup_data_api(elt)
+        #print (str(data_from_cat))
+        for data in data_from_cat["products"]:
+            if "categories" in data and "en:" in "categories":
+                pass
+            else:
+                try:
+                    category = cl.Categories(data)
+                    if "en:" in category.name or "fr:" in category.name:
+                        pass
+                    else:
+                        cursor.execute("INSERT INTO Categories (id, name)" \
+                                       "VALUES (%s, %s)", (category.id, category.name))
+                        conn.commit()
+                        print("Catégorie correctement insérée")
+                # Don't take the non utf-8 data
+                except (conn.OperationalError, conn.DataError, conn.IntegrityError, KeyError):
+                    pass
+
+
         
-            
+
+
+def lister_url():
+    data_from_list = {}
+    for elt in CATEGORIES:  # pour chaque elt de ma liste
+        # print(elt)
+        for i in range(1, 10):  # generation d'un boucle avec i comme iterateur de 0 à 1133
+            nouvelle_liste_url.append('{0}{1}.json'.format(elt, str(i)))  # formatage des url 1133 fois(nombre de pages max dans une catégories)\
+            # + ajout à nouvelle liste
+            #print(str(nouvelle_liste_url))
+    for elt in nouvelle_liste_url:  # pour chaque elt de
+        print("récupération des produits en cours")
+        recup_data_api(elt)
+        return data_from_list
+    print("Récupération terminée")
+
+
+def insert_product():
+    lister_url()
+    for elt in nouvelle_liste_url:
+        print(elt)
+        data_from_list = recup_data_api(elt)        
+        
+        for data in data_from_list["products"]:
+            try:
+                nutri_values = ["a", "b", "c", "d", "e"]
+                nutri_score = []
+                for nutri_values in data_from_list["nutrition_grade_fr"]:
+                    nutri_score.append(nutri_values.upper())
+                return food.nutri_score
+            except KeyError:
+                str_nutr_grade = "N/A"
+                nutriscore = 0
+                pass
+            try:
+                food = cl.Food(data)
+                cursor.execute("INSERT INTO Food (name, categories_id, nutri_score, url, stores)" \
+                               "VALUES (%s, %s, %s, %s, %s)",
+                               (food.name, food.category, food.nutri_score, food.url, food.stores))
+                conn.commit()
+                print("Produit correctement insérée")
+            except conn.OperationalError:  # Don't take the products with encoding error
+                pass
+            except KeyError:  # Don't take lignes without 'product_name'
+                pass
+            except AttributeError:  # Don't take products with 0 categories
+                pass
+            except conn.IntegrityError:  # Don't take the products with an category unknow in the database
+                pass
+            except conn.DataError:  # Pass when product name is too long
+                pass
 
 
 def main():
     """Main function, lauching the script"""
-    #Call the sql script to create the database
+    # Call the sql script to create the database
     cursor.execute(FILE_CREATE)
     print("Base de données Openfoodfacts créé avec succès !")
 
     cursor.execute('use openfoodfacts;')
-    fill_categories_table(URL2)
-    for i in range(1, 1724): #1724 Nombre de page de la catégorie
-        url_food = URL0+str(i)+'.json'
-        insert_product(url_food)
+    remplir_table_categorie()
+    insert_product()
     print("Base de données Openfoodfacts remplie avec succès !")
 
 
-                
+
 main()
-
-
-
